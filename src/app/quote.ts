337afb292bcdf1a977470905c992368ce641817a
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-22 13:37:28 +0300

    Adds a date property to the class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     viewDetails:boolean;
-    constructor(public submitter:string,public author:string,public quote:string,public upvote:number,public downvote:number){
+    constructor(public submitter:string,public author:string,public quote:string,public upvote:number,public downvote:number,public elapse :Date){
         this.viewDetails = false;
 
     }
