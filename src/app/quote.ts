e7fb7551f63d61245b2bd41a6629003af037e708
danielevans-999 <danielevans.karani@gmail.comgit>
2019-09-22 15:52:36 +0300

    Changes the submitter argument to name

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     viewDetails:boolean;
-    constructor(public submitter:string,public author:string,public quote:string,public upvote:number,public downvote:number,public elapse :Date){
+    constructor(public name:string,public author:string,public quote:string,public upvote:number,public downvote:number,public elapse :Date){
         this.viewDetails = false;
 
     }
